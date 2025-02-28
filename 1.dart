685cb5918722331dce39a19b000eec64105ad889
lThiag0 <thiaguinhofurtado07@hotmail.com>
2025-02-28 19:45:04 -0300

    Questão 1 feita por Rian Santos

--- 1.dart
+++ 1.dart
@@ -1,1 +1,21 @@
 // Atividade 01: Validação de categorias de produtos em um sistema de estoque.
+
+import 'dart:io';
+
+void main() {
+  String entrada = "";
+  List<String> categorias = <String>["eletronicos", "alimentos", "vestuario", "livros"];
+
+  void validar() {
+    print('Digite a categoria do produto (${categorias.toString()}): ');
+    entrada = stdin.readLineSync()!;
+    if (categorias.contains(entrada)) {
+      print('Categoria válida: ${entrada}');
+    } else {
+      print('Categoria invalida: ${entrada}');
+    }
+  }
+
+  validar();
+
+}
