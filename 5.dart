a3ee19c28e48a5cd8ee6319b0200eed2e6b771ca
Carlos Ellyhoram <ellyhoramce6@gmail.com>
2025-02-28 20:14:15 -0300

    Questão 5 feita por Carlos Ellyhoram

--- 5.dart
+++ 5.dart
@@ -1,1 +1,22 @@
 // Atividade 05: Validação de métodos de pagamento em uma plataforma de e-commerce
+
+import 'dart:io';
+
+void main() {
+  String entrada = "";
+  List<String> metados = <String>["cartao", "boleto", "paypal", "pix"];
+
+  void validar() {
+    print('Escolha o método de pagamento desejado (${metados.toString()}): ');
+    entrada = stdin.readLineSync()!;
+    if (metados.contains(entrada)) {
+      print('Formato de pagamento é válido: ${entrada}');
+    } else {
+      print('Formato de pagamento é invalido [${entrada}], tente um novo formato de pagamento!');
+      validar();
+    }
+  }
+
+  validar();
+
+}
