d2fd5ef59f728f55c51955645e6ca06fece3522d
Rian Santos <riammarreta735@gmail.com>
2025-02-28 19:59:19 -0300

    Update da questão 1 com comentarios

--- 1.dart
+++ 1.dart
@@ -3,12 +3,16 @@
 import 'dart:io';
 
 void main() {
+  // Inicia a variavel com o nome entrada e cria a lista ja com itens existentes
   String entrada = "";
   List<String> categorias = <String>["eletronicos", "alimentos", "vestuario", "livros"];
 
+  // Cria a função para validr 
   void validar() {
+    // Pede ao usuario para digita e salva na entrada a resposta
     print('Digite a categoria do produto (${categorias.toString()}): ');
     entrada = stdin.readLineSync()!;
+    // Verifica se existe o valor digitado na lista
     if (categorias.contains(entrada)) {
       print('Categoria válida: ${entrada}');
     } else {
