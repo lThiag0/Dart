15c3466dbe5687c7b8efe355a1dd9fd4ae6ad351
Gabriel Lopes <gabriellopescosta2003@gmail.com>
2025-02-28 20:06:48 -0300

    Questão 2 feita por Gabriel Lopes

--- 2.dart
+++ 2.dart
@@ -1,1 +1,22 @@
 // Atividade 02: Validação de tipos de arquivos em um sistema de upload
+
+import 'dart:io';
+
+void main() {
+  String entrada = "";
+  List<String> formatos = <String>["pdf", "jpg", "png", "docx"];
+
+  void validar() {
+    print('Digite o tipo de formato do arquivo (${formatos.toString()}): ');
+    entrada = stdin.readLineSync()!;
+    if (formatos.contains(entrada)) {
+      print('Formato de arquivo é válido: ${entrada}');
+    } else {
+      print('Formato de arquivo é invalido [${entrada}], tente um novo formato de arquivo!');
+      validar();
+    }
+  }
+
+  validar();
+
+}
